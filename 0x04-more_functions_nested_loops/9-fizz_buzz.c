c258c422151569e2c445454833f29584d501ee0b
abbysta <abbystadoc@gmail.com>
2023-04-28 06:02:27 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,9 +18,9 @@ for (i = 1; i <= 100; i++)
 if (i % 15 == 0)
 printf("FizzBuzz");
 else if (i % 3 == 0)
-printf("FizzBuzz");
+printf("Fizz");
 else if (i % 5 == 0)
-printf("FizzBuzz");
+printf("Buzz");
 else
 printf("%i", i);
 if (i < 100)
